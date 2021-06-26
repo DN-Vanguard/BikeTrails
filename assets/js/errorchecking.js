e6df0fe583b28aa5625f616be42c43322c6fef24
DN-Vanguard <80746945+DN-Vanguard@users.noreply.github.com>
2021-06-26 00:32:09 -0700

    Revert "Georgem"

--- assets/js/errorchecking.js
+++ assets/js/errorchecking.js
@@ -1,4 +1,3 @@
-// Error checking functions - checks to see if object is a number
 // global variables
 var mmodal1 = document.getElementsByClassName("modal1")[0];
 var mmodal2 = document.getElementsByClassName("modal2")[0];
@@ -13,52 +12,25 @@ function isNumeric(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-// Function to check bike trail inputs mininum,maximum distance and search radius
 // If user does not enter input for any of the 3 inputs, modal will appear
 function ErrorBikeinputschk(mindist,maxdist,radius){ 
-    if(!isNumeric(radius)||radius==null || radius > 50 || radius <0.1 ){
-        presentmodal1();
+    console.log("Error Checking inprogress")
+    if(!isNumeric(radius)||radius==null || radius > 50 ){
+        console.log("issue with radius")
+        // console.log("Error Checking inprogress radius",modal1.getattribute("style"))
+        document.getElementsByClassName('modal1')[0].style.display ="block";
         return(false);
     }
+    // mindist=parseFloat(mindistEl.val())
+    // maxdist=parseFloat(maxdistEl.val())
     var badinputs = (!isNumeric(mindist) || !isNumeric(maxdist)||mindist > maxdist || mindist <0 )
     if(badinputs){
-        presentmodal1();
+		// console.log("modal called")
+        mmodal1.style.display = "block";
 		return false;
 	}
-    else{ 
-        return true
-    }
+    else{ return true}
 }
-
-function presentmodal1(){
-   
-    $("header").append(`
-    <div id="mmd" class="modal left">
-        <div  id = "mmd1" class="modal1">
-            <div class="modal-header " >
-                <h3 style="color:white">Error in Bike Trail Information Entry</h3>
-                <span id = "spann1" class="close">&times;</span>
-            </div>
-                <div class="modal-content">
-                <p style="color:black">1) Check Search Radius is a number and 0 &lt; Radius &lt; 50 miles </p>   
-                <p style="color:black">2) Check Min Trail length and Max Trail length are numbers </p>
-                <p style="color:black">3) Check that: 0 &lt; Min Trail length &lt; Max Trail length</p>
-                </div>
-        </div> 
-    </div>
-    `)
-    var span = document.getElementById("spann1");
-    span.addEventListener('click',function(e){
-        e.preventDefault();
-        // document.getElementsByClassName('modal1')[0].style.display = "none";
-        window.location.reload();
-    })
-}
-// Modal close for incorrect trail inputs  radius, mindist, maxdist
-
-
-
-// Resize window for mobile  - desktop view dynamically 
 // To exit out of modal
 span.addEventListener('click',function(e){
     console.log("span on modal1")
@@ -82,23 +54,21 @@ $(window).on('resize', function() {
       $('main').addClass('col');
     }
   });
-// Toggles trail info description from truncate to full 
+
   var traillsdis = document.getElementById("trails-display")
   traillsdis.addEventListener("click",function(event){
-    var element=event.target;
-    if(element.classList[0]==="truncate"){
-        element.setAttribute("class","normal");
-        }
-        else{
-            element.setAttribute("class","truncate");
-        }
-    })
+  console.log("clicked on card")
+  var element=event.target;
+  console.log("first", element.classList)
+  if(element.classList[0]==="truncate"){
+      var state = element.getAttribute("data-state");
+      element.setAttribute("class","");
+      }
+      else{
+        element.setAttribute("class","truncate");
+      }
+      console.log("second", element.classList)
+  })
+      
 
-// Start window in correct class
-if(window.innerWidth > 600){
-    $('main').addClass('row');
-}
-else{
-    $('main').addClass('col');
-}
 
